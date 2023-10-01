0e9cb0e9e688f91022e8813ac8011965ea43963b
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2023-10-01 13:07:05 +0530

    fix: remove unnecessary processing

--- app.py
+++ app.py
@@ -43,31 +43,27 @@ def get_embedding(text, model="text-embedding-ada-002"):
     text = text.replace("\n", " ")
     return openai.Embedding.create(input = [text], model=model)['data'][0]['embedding']
 
-
+l_df = pandas.read_csv(f"embs0.csv")
+embs = []
+for r1 in range(len(l_df.embedding)): # Changing the format of the embeddings into a list due to a parsing error
+    e1 = l_df.embedding[r1].split(",")
+    for ei2 in range(len(e1)):
+        e1[ei2] = float(e1[ei2].strip().replace("[", "").replace("]", ""))
+    embs.append(e1)
+l_df["embedding"] = embs
+print("Loaded embeddings")
 
 def logic(question):
-    
-    df = pandas.read_csv(f"embs0.csv")
-
-    embs = []
-    for r1 in range(len(df.embedding)): # Changing the format of the embeddings into a list due to a parsing error
-        e1 = df.embedding[r1].split(",")
-        for ei2 in range(len(e1)):
-            e1[ei2] = float(e1[ei2].strip().replace("[", "").replace("]", ""))
-        embs.append(e1)
-
-    df["embedding"] = embs
-
-    bot_message = ""
+    df = l_df.copy(deep=True)
     product_embedding = get_embedding( # Creating an embedding for the question that's been asked
         question
     )
     df["similarity"] = df.embedding.apply(lambda x: cosine_similarity(x, product_embedding)) # Finds the relevance of each piece of data in context of the question
-    df.to_csv("embs0.csv")
+    # df.to_csv("embs0.csv")
 
     df2 = df.sort_values("similarity", ascending=False) # Sorts the text chunks based on how relevant they are to finding the answer to the question
-    df2.to_csv("embs0.csv")
-    df2 = pandas.read_csv("embs0.csv")
+    # df2.to_csv("embs0.csv")
+    # df2 = pandas.read_csv("embs0.csv")
     #print(df2["similarity"][0])
 
     from langchain.docstore.document import Document
